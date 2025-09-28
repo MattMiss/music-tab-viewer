eaa79ccf19d361262d54814b10a4ff39f84c00e8
Matt Miss <matt.w.miss@gmail.com>
2025-09-28 16:12:23 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <div className="min-h-screen grid place-items-center bg-slate-900 text-white">
+            <h1 className="text-4xl font-bold">Music Tab Viewer 🎶</h1>
+        </div>
+    );
+}
